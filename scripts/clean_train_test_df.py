c66c0319f89d9edc9cb5b8dcd416f85946abac76
Melak2017 <melakuandarge615@gmail.com>
2022-05-28 05:47:19 +0300

    add optimizer function

--- scripts/clean_train_test_df.py
+++ scripts/clean_train_test_df.py
@@ -231,3 +231,32 @@ class CleanStoreDf:
 
         except:
             print("Failed to save data")
+
+    def optimize_df(self) -> pd.DataFrame:
+        """
+        Returns the DataFrames information after all column data types are optimized (to a lower data type)
+        Parameters
+        ----------
+        None
+        Returns
+        -------
+        pd.DataFrame
+        """
+        data_types = self.df.dtypes
+        optimizable = ['float64', 'int64']
+        try:
+            for col in data_types.index:
+                if(data_types[col] in optimizable):
+                    if(data_types[col] == 'float64'):
+                        # downcasting a float column
+                        self.df[col] = pd.to_numeric(
+                            self.df[col], downcast='float')
+                    elif(data_types[col] == 'int64'):
+                        # downcasting an integer column
+                        self.df[col] = pd.to_numeric(
+                            self.df[col], downcast='unsigned')
+
+            return self.df
+
+        except:
+            print('Failed to optimize')
